f4faa78fad9d9aa8b3b1410e86a9354437c2fd3b
Kristina Alabova <kristina.alabova@mail.ru>
2025-05-29 19:40:04 +0300

    fix nginx

--- backend/foodgram/models.py
+++ backend/foodgram/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import MinValueValidator, RegexValidator
+from django.core.validators import MinValueValidator
+# , RegexValidator
 from django.db import models
 
 from .constants import (MAX_EMAIL, MAX_INGREDIENTS, MAX_MEASUREMENT_UNIT,
