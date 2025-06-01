ee431bcd6bae86228e626012adcacb499ef828f7
Kristina Alabova <kristina.alabova@mail.ru>
2025-06-01 21:56:41 +0300

    check statuses

--- backend/api/views.py
+++ backend/api/views.py
@@ -40,7 +40,7 @@ class CustomUserViewSet(viewsets.ModelViewSet):
         serializer = CustomUserCreateSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     @action(
