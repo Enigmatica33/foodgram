85426d002872d8ec707c9956871e9d92c389fdc6
Kristina Alabova <kristina.alabova@mail.ru>
2025-06-02 11:49:03 +0300

    short_link

--- backend/api/views.py
+++ backend/api/views.py
@@ -28,8 +28,8 @@ from .serializers import (AvatarSerializer, CustomUserCreateSerializer,
                           RecipeSerializer, TagListSerializer)
 
 
-def redirect_short_link(request, short_link):
-    recipe = get_object_or_404(Recipe, short_link=short_link)
+def redirect_short_link(request):
+    recipe = get_object_or_404(Recipe, short_link='c81')
     return redirect(f'/recipes/{recipe.id}/')
 
 
--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path(
-        's/<str:short_link>/',
+        's/c81',
         redirect_short_link,
         name='redirect_short_link'
     )
