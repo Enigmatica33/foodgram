357b5fbeacf806d9cdb8b128617c6c620d3eae5d
Kristina Alabova <kristina.alabova@mail.ru>
2025-06-04 12:29:52 +0300

    pdf.py

--- backend/api/pdf.py
+++ backend/api/pdf.py
@@ -11,16 +11,20 @@ def pdf_creating(self, ingredients, username):
     buffer = BytesIO()
     p = canvas.Canvas(buffer, pagesize=letter)
     width, height = letter
+    # Регистрируем шрифт и устанавливаем его размер для всего документа.
     pdfmetrics.registerFont(TTFont('DejaVuSans', 'DejaVuSans.ttf'))
-    p.setFont('DejaVuSans', 16)
-    p.drawString(100, height - 50, 'Список покупок')
-    y_position = height - 100
+    p.setFont('DejaVuSans', 12)
+    # Устанавливаем заголовок документа
     p.setFont('DejaVuSans', 20)
+    p.drawString(100, height - 50, 'Список покупок')
+    # Начальные параметры для размещения текста
+    y_position = height - 100
+    text_offset = 30  # Смещение между строками
     for ingredient in ingredients:
         text = f"-{ingredient['ingredient__name']}: {ingredient['amount']}"
         f"{ingredient['ingredient__measurement_unit']}"
         p.drawString(100, y_position, text.encode('utf-8').decode('utf-8'))
-        y_position -= 30
+        y_position -= text_offset
         if y_position < 50:
             p.showPage()
             p.setFont('DejaVuSans', 12)
