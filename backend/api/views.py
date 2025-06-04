b12963e3e0b479694df4ee294c317fee520f9c0a
Kristina Alabova <kristina.alabova@mail.ru>
2025-06-04 11:05:53 +0300

    favorite

--- backend/api/views.py
+++ backend/api/views.py
@@ -222,7 +222,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
     @action(
         detail=True,
         methods=['post', 'delete'],
-        # permission_classes=(IsAuthenticated,)
+        url_path='favorite',
+        url_name='favorite',
+        permission_classes=(IsAuthenticated,)
     )
     def get_favorite(self, request, pk=None):
         """Добавление рецепта в избранное."""
