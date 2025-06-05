c54492c824aedccf3ba14c65df207238a65f728b
Kristina Alabova <kristina.alabova@mail.ru>
2025-06-05 21:04:42 +0300

    cors

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -65,7 +65,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram_backend.wsgi.application'
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_URLS_REGEX = r'^/(api|admin)/.*$'
+CORS_URLS_REGEX = r'^.*$'
 
 
 # Database
