fc4b87d380719619c8716a764d657fb125867beb
Kristina Alabova <kristina.alabova@mail.ru>
2025-06-05 19:07:15 +0300

    favorite and shopping cart

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -1,15 +1,15 @@
-from api import views
+# from api import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path(
-        's/<str:short_link>',
-        views.redirect_short_link,
-        name='redirect_short_link'
-    ),
+    # path(
+    #     's/<str:short_link>',
+    #     views.redirect_short_link,
+    #     name='redirect_short_link'
+    # ),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
