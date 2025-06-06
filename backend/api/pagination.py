7558d39b3a840c6c58d8b927e3ef5a9dbf560ea8
Kristina Alabova <kristina.alabova@mail.ru>
2025-06-06 19:00:23 +0300

    pagination

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -3,6 +3,6 @@ from rest_framework.pagination import PageNumberPagination
 
 class CustomPagination(PageNumberPagination):
     """Пагинатор для вывода определённого количества элементов на странице."""
-    page_size_query_param = 'limit'  # Параметр для указания количества объектов на странице
-    page_query_param = 'page'        # Параметр для указания номера страницы
-    max_page_size = 100              # Максимальное количество объектов на странице
+    page_size_query_param = 'limit'  # количество объектов на странице
+    page_query_param = 'page'        # номер страницы
+    max_page_size = 100              # Макс. объектов на странице
