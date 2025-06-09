ab64e57dc59ef26a5c8eb26184d9fb90f1298f0b
Kristina Alabova <kristina.alabova@mail.ru>
2025-06-09 10:22:57 +0300

    nginx redirect short_link

--- backend/foodgram/models.py
+++ backend/foodgram/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator, RegexValidator
 from django.db import models
-from django.urls import reverse
 
 from .constants import (MAX_EMAIL, MAX_INGREDIENTS, MAX_MEASUREMENT_UNIT,
                         MAX_RECIPE_NAME, MAX_TAG, MAX_USER)
