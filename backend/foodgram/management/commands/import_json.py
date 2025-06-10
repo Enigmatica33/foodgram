343e7f7f8c2b9c5a6adad9385d1c5ab302a6e399
Kristina Alabova <kristina.alabova@mail.ru>
2025-06-10 21:00:32 +0300

    import_json command

--- backend/foodgram/management/commands/import_json.py
+++ backend/foodgram/management/commands/import_json.py
@@ -1,5 +1,7 @@
 import json
+import os
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from foodgram.models import Ingredient
@@ -8,40 +10,51 @@ from foodgram.models import Ingredient
 class Command(BaseCommand):
     help = 'Импорт ингредиентов из файла JSON'
 
-    def add_arguments(self, parser):
-        parser.add_argument(
-            'json_file',
-            type=str,
-            help='Файл для импорта'
-        )
-
     def handle(self, *args, **kwargs):
-        json_file = kwargs['json_file']
-
+        json_file_path = os.path.join(
+            settings.BASE_DIR,
+            'ingredients.json'
+        )
         try:
-            with open(json_file, 'r', encoding='utf-8') as f:
+            with open(json_file_path, 'r', encoding='utf-8') as f:
                 data = json.load(f)
-
-                for item in data:
-                    ingredient, created = Ingredient.objects.get_or_create(
-                        name=item['name'],
-                        defaults={
-                            'measurement_unit': item['measurement_unit']
-                        }
-                    )
-                    if created:
-                        self.stdout.write(self.style.SUCCESS(
-                            f'Ингредиент {ingredient.name} добавлен.'
-                        ))
-                    else:
-                        self.stdout.write(self.style.WARNING(
-                            f'Ингредиент {ingredient.name} уже существует.'
-                        ))
         except FileNotFoundError:
-            self.stderr.write(self.style.ERROR('Файл не найден.'))
+            self.stderr.write(
+                self.style.ERROR(f'Файл {json_file_path} не найден.')
+            )
+            return
         except json.JSONDecodeError:
             self.stderr.write(
                 self.style.ERROR('Ошибка в процессе декодирования JSON.')
             )
+            return
+
+        ingredients_to_create = []
+        for item in data:
+            name = item.get('name')
+            measurement_unit = item.get('measurement_unit')
+            if name and measurement_unit:
+                ingredients_to_create.append(
+                    Ingredient(
+                        name=str(name).strip(),
+                        measurement_unit=measurement_unit
+                    )
+                )
+        if not ingredients_to_create:
+            self.stdout.write(
+                self.style.WARNING(
+                    'Нет валидных ингредиентов '
+                    'для добавления после обработки JSON.'))
+            return
+        try:
+            created_ingredients = Ingredient.objects.bulk_create(
+                ingredients_to_create, 
+                ignore_conflicts=True
+            )
+            self.stdout.write(
+                self.style.SUCCESS('Загрузка ингредиентов успешно завершена.')
+            )
         except Exception as e:
-            self.stderr.write(self.style.ERROR(f'Ошибка: {e}'))
+            self.stderr.write(
+                self.style.ERROR('Произошла непредвиденная ошибка при '
+                                 f'сохранении ингредиентов:: {e}'))
