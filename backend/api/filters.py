0935cc7240fad16f359f56a5a0497cc1d94f98d6
Kristina Alabova <kristina.alabova@mail.ru>
2025-06-13 19:08:20 +0300

    fix errors

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -31,7 +31,8 @@ class RecipeFilter(filters.FilterSet):
 
     # def is_in_shopping_cart_method(self, queryset, name, value):
     #     if value and self.request.user.is_authenticated:
-    #         return queryset.filter(recipe_shoppingcart__user=self.request.user)
+    #         return queryset.filter(
+    # recipe_shoppingcart__user=self.request.user)
     #     return queryset
 
     class Meta:
